ddf04430f6c0a368eb0d6bca7d376888f532d498
bidinzky <binder.lukas@outlook.com>
2022-04-02 19:33:02 +0000

    added comments for 02_04_2022 palindrom.py

--- 02_04_2022/palindrom.py
+++ 02_04_2022/palindrom.py
@@ -1,12 +1,12 @@
 
 def checkPalindrom(n):
     reversed_num = 0
-    num = n
+    num = n #copy n into num, because we modify num but we need n for the comparision later
     while num != 0:
-        digit = num % 10
-        reversed_num = reversed_num * 10 + digit
-        num //= 10
-    return reversed_num == n
+        digit = num % 10 #get the lowest digit of the number e.g. 1234 % 10 = 4 
+        reversed_num = reversed_num * 10 + digit #shift all existing digits to the right (*10) and the newest one
+        num //= 10 # integer divide by 10 to 1234 // 10 = 123 --> ignores decimal points
+    return reversed_num == n #check if reversed number equals original
 
 print(checkPalindrom(12021))
 print(checkPalindrom(1231))
