0c2b550d59d1e5ef0cdbad3f6b5b42170a46da76
Lukas Binder <binder.lukas@outlook.com>
2022-04-24 13:31:10 +0200

    add postfix calculator

---
+++ 24_04_2022/main.py
@@ -0,0 +1,6 @@
+import postfixEvaluation
+
+if __name__ == '__main__':
+    print(postfixEvaluation.evaluate("13 15 + 5 * ="))
+
+
---
+++ 24_04_2022/postfixEvaluation.py
@@ -0,0 +1,28 @@
+def evaluate(text: str) -> int:
+    stack: list[int] = []
+    tokens = text.split(" ")
+    for token in tokens:
+        if token.isnumeric():
+            stack.append(int(token))
+        elif token in "+-*/":
+            right = stack.pop()
+            left = stack.pop()
+            stack.append(compute(left, token, right))
+        elif token == '=':
+            return stack.pop()
+        else:
+            raise ValueError("unknown token {0} found".format(token))
+
+
+def compute(left: int, op: str, right: int) -> int:
+    match op:
+        case "+":
+            return left + right
+        case "-":
+            return left - right
+        case "*":
+            return left * right
+        case "/":
+            return left // right
+        case _:
+            raise ValueError("op {0} unknown".format(op))
