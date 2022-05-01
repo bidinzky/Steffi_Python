d8d5c9dcf8d60fc4eb76dfe20482f56604bf8bc9
Lukas Binder <binder.lukas@outlook.com>
2022-05-01 18:58:41 +0200

    add main with types

---
+++ 25_04_2022/main_with_types.py
@@ -0,0 +1,72 @@
+import collections
+import math
+import statistics
+from typing import Deque
+
+
+class SensorValue:
+    def __init__(self, value: float, time: str):
+        self.value = value
+        self.time = time
+
+    def __repr__(self):
+        return f"SensorValue({self.value}, {self.time})"
+
+
+class Datastore:
+    def __init__(self, max_size: int):
+        self.data: Deque[SensorValue] = collections.deque(maxlen=max_size)
+
+    def addValue(self, sv: SensorValue):
+        self.data.append(sv)
+
+    def getAverage(self):
+        return statistics.mean((sv.value for sv in self.data))
+
+    def getMax(self):
+        return max(self.data, key=lambda sv: sv.value)
+
+    def getMin(self):
+        return min(self.data, key=lambda sv: sv.value)
+
+    def printValue(self):
+        print("==========================")
+        print("Average:", self.getAverage())
+        print("Max:", self.getMax())
+        print("Min:", self.getMin())
+
+
+class ControlSystem(Datastore):
+    def __init__(self, max_size: int, min_threshold: float = -math.inf, max_threshold: float = math.inf):
+        super().__init__(max_size)
+        self.min_threshold = min_threshold
+        self.max_threshold = max_threshold
+
+    def addValue(self, sv: SensorValue):
+        if not self.min_threshold <= sv.value <= self.max_threshold:
+            raise Exception(
+                f"Value: {sv.value} is outside of thresholds min: {self.min_threshold} and max: {self.max_threshold}")
+        super().addValue(sv)
+
+
+if __name__ == '__main__':
+    c = ControlSystem(5, min_threshold=5, max_threshold=25)
+    # c = Datastore(5)
+    c.addValue(SensorValue(10, "20:00"))
+    c.printValue()
+    c.addValue(SensorValue(15, "20:01"))
+    c.printValue()
+    c.addValue(SensorValue(20, "20:02"))
+    c.printValue()
+    c.addValue(SensorValue(21, "20:03"))
+    c.printValue()
+    c.addValue(SensorValue(22, "20:04"))
+    c.printValue()
+    c.addValue(SensorValue(23, "20:05"))
+    c.printValue()
+    c.addValue(SensorValue(24, "20:06"))
+    c.printValue()
+    c.addValue(SensorValue(25, "20:07"))
+    c.printValue()
+    c.addValue(SensorValue(26, "20:08"))
+    c.printValue()
