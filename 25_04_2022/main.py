330dd12025d0ca5e9776dde188050bf33916f8bf
bidinzky <binder.lukas@outlook.com>
2022-05-01 14:12:53 +0000

    some changes

--- 25_04_2022/main.py
+++ 25_04_2022/main.py
@@ -1,5 +1,44 @@
-from collector import CollectorGuard
-from collector import Collector
+import collections
+import statistics
+
+class SensorValue:
+    def __init__(self, value, time):
+        self.value = value
+        self.time = time
+
+class Datastore:
+    def __init__(self, max_size):
+        self.data = collections.deque(maxlen=max_size)
+
+    def addValue(self, sv):
+        self.data.append(sv)
+    
+    def getAverage(self):
+        return statistics.mean((o.value for o in self.data))
+
+    def getMax(self):
+        return max(self.temp_data, key=lambda x: x.value)
+    
+    def getMin(self):
+        return min(self.temp_data, key=lambda x: x.value)
+    
+    def printValue(self):
+        print("==========================")
+        print("Average:", self.getAverage())
+        print("Max:", self.getMax())
+        print("Min:", self.getMin())
+        
+
+class CollectorGuard(Collector):
+    def __init__(self, max_size, threshold):
+        super().__init__(max_size)
+        self.threshold = threshold
+        
+    def addValue(self, temp, time):
+        if temp > self.threshold:
+            raise Exception("Value is too high")
+        super().addValue(temp, time)
+
 if __name__ == '__main__':
     #c = CollectorGuard(5, 25)
     c = Collector(5)
